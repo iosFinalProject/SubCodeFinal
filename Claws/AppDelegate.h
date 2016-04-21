70c2f7d555d46d5fe79b35050447f8368eb70fd0
Goss <libbyjayne@hotmail.com>
2016-04-21 18:38:16 -0400

    Initial Commit

---
+++ Claws/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Claws
+//
+//  Created by Libby Goss on 4/21/16.
+//  Copyright © 2016 nyu.edu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Claws/GameViewController.h
@@ -0,0 +1,14 @@
+//
+//  GameViewController.h
+//  Claws
+//
+
+//  Copyright (c) 2016 nyu.edu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameViewController : UIViewController
+
+@end
